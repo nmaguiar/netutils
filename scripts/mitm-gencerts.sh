bde0fa0112a21ef994789cbebf4d84c334ad9c92
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2024-08-07 03:47:35 +0000

    chore: Update Dockerfile to add missing dependencies and improve readability

--- scripts/mitm-gencerts.sh
+++ scripts/mitm-gencerts.sh
@@ -18,4 +18,11 @@ cd ~/.mitmproxy
 tar czf /tmp/mitmcerts.tgz . 
 CERTS=$(cat /tmp/mitmcerts.tgz | base64 -w 0)
 
+echo "---"
+echo "copy the following command from the clipboard to another file (editing with vi, for example):"
+echo ""
 echo "echo $CERTS | base64 -d | tar xz"
+echo ""
+echo "---"
+echo "Then you can execute the file, as shell script, to get the certificates in another pod or container"
+echo ""
