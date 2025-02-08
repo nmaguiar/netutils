0eb1dd0438e179389a537d7090d630e5643a07cb
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2025-02-08 02:31:19 +0000

    Update switch-user-by-pid.sh

--- scripts/switch-user-by-pid.sh
+++ scripts/switch-user-by-pid.sh
@@ -28,6 +28,16 @@ fi
 USER=$(echo $PROCESS_INFO | awk '{print $2}')
 GROUP=$(echo $PROCESS_INFO | awk '{print $3}')
 
+# Create the group if it doesn't exist
+if ! getent group "$GROUP" > /dev/null 2>&1; then
+  groupadd "$GROUP"
+fi
+
+# Create the user if it doesn't exist
+if ! getent passwd "$USER" > /dev/null 2>&1; then
+  useradd -g "$GROUP" "$USER"
+fi
+
 # Switch to the user and group of the process
 echo "Switching to user '$USER' and group '$GROUP' based on process with PID $PID."
 sudo -g $GROUP -u $USER /bin/sh -c "cd /proc/$PID/root && bash"
