7ee2a8ce5ee1115a47d880d7694fff555a010e63
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2025-03-21 13:29:19 +0000

    Enhance user and group handling in switch-user-by-pid.sh and switch-fs-by-pid.sh scripts

--- scripts/switch-fs-by-pid.sh
+++ scripts/switch-fs-by-pid.sh
@@ -6,12 +6,49 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+# Check if the script is executed as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 PID=$1
-CWD="/proc/$PID/cwd"
+CWD="/proc/$PID"
 
 if [ ! -d "$CWD" ]; then
-    echo "Invalid PID or the process does not have a current working directory."
+    echo "Invalid PID or the process does not have a root filesystem."
     exit 1
 fi
 
-sudo chroot "$CWD" /bin/bash -c 'echo "Warning: Type exit to return back to the original filesystem"; exec /bin/bash'
+# Get the user and group of the process
+USER=$(ps -o user= -p "$PID")
+GROUP=$(ps -o group= -p "$PID")
+
+# If the extracted username is numeric, prepend a "u" to avoid invalid user names
+if [[ "$USER" =~ ^[0-9]+$ ]]; then
+  USER="u$USER"
+fi
+
+# If the extracted group is numeric, prepend a "u" to avoid invalid group names
+if [[ "$GROUP" =~ ^[0-9]+$ ]]; then
+  GROUP="u$GROUP"
+fi
+
+# Create the group if it doesn't exist
+if ! getent group "$GROUP" > /dev/null 2>&1; then
+  groupadd "$GROUP"
+fi
+
+# Create the user if it doesn't exist
+if ! getent passwd "$USER" > /dev/null 2>&1; then
+  if command -v useradd > /dev/null 2>&1; then
+    useradd -m -g "$GROUP" "$USER"
+  elif command -v adduser > /dev/null 2>&1; then
+    adduser -m --ingroup "$GROUP" "$USER"
+  else
+    echo "Neither useradd nor adduser command is available."
+    exit 1
+  fi
+fi
+
+sudo -g $GROUP -u $USER -i bash -c "cd $CWD/root; exec /bin/bash"
--- scripts/switch-user-by-pid.sh
+++ scripts/switch-user-by-pid.sh
@@ -28,6 +28,16 @@ fi
 USER=$(echo $PROCESS_INFO | awk '{print $2}')
 GROUP=$(echo $PROCESS_INFO | awk '{print $3}')
 
+# If the extracted username is numeric, prepend a "u" to avoid invalid user names
+if [[ "$USER" =~ ^[0-9]+$ ]]; then
+  USER="u$USER"
+fi
+
+# If the extracted group is numeric, prepend a "u" to avoid invalid group names
+if [[ "$GROUP" =~ ^[0-9]+$ ]]; then
+  GROUP="u$GROUP"
+fi
+
 # Create the group if it doesn't exist
 if ! getent group "$GROUP" > /dev/null 2>&1; then
   groupadd "$GROUP"
@@ -36,9 +46,9 @@ fi
 # Create the user if it doesn't exist
 if ! getent passwd "$USER" > /dev/null 2>&1; then
   if command -v useradd > /dev/null 2>&1; then
-    useradd -g "$GROUP" "$USER"
+    useradd -m -g "$GROUP" "$USER"
   elif command -v adduser > /dev/null 2>&1; then
-    adduser --ingroup "$GROUP" "$USER"
+    adduser -m --ingroup "$GROUP" "$USER"
   else
     echo "Neither useradd nor adduser command is available."
     exit 1
@@ -48,4 +58,4 @@ fi
 # Switch to the user and group of the process
 echo "Switching to user '$USER' and group '$GROUP' based on process with PID $PID."
 #sudo -g $GROUP -u $USER -- /bin/sh -c "cd /proc/$PID/root && bash"
-sudo -g $GROUP -u $USER -i
+sudo -g $GROUP -u $USER -i bash
