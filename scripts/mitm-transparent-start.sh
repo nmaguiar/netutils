b45d4df22496e1cfea0279630f97fc2ab3f8c875
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2025-06-05 12:37:26 +0000

    Refactor mitm-transparent-start.sh to conditionally use mitmweb based on argument

--- scripts/mitm-transparent-start.sh
+++ scripts/mitm-transparent-start.sh
@@ -2,5 +2,11 @@
 
 MITM_PORT="${MITM_PORT:-8080}"
 
-# Start mitmproxy in transparent mode
-sudo -u mitm mitmproxy --mode transparent --showhost -p "$MITM_PORT"
+# If a port number is provided as an argument, use mitmweb with --web-host 0.0.0.0
+if [ -n "$1" ]; then
+  MITMWEB_PORT="$1"
+  sudo -u mitm mitmweb --mode transparent --showhost -p "$MITM_PORT" --web-host 0.0.0.0 --web-port "$MITMWEB_PORT"
+else
+  # Start mitmproxy in transparent mode
+  sudo -u mitm mitmproxy --mode transparent --showhost -p "$MITM_PORT"
+fi
